e3236c140fa9807d8668ea0f81c2051a02befa17
Namohysip <higa.noah@gmail.com>
2016-09-27 14:00:42 -1000

    Create DAGUtilities.cpp
    Utility function used to print out nodes and edges of a given graph, using the 'id' and 'runtime' graphs.
    
    TODO: Rewrite "newJob" because it's useless in its current state.

---
+++ C/DAGUtilities.cpp
@@ -0,0 +1,81 @@
+#include"defun.hpp"
+#include <iostream>
+#include <igraph.h>
+
+Job newJob(int id, int runtime){
+	struct Job aNewJob;
+	aNewJob.id = id;
+	aNewJob.runtime = runtime;
+	return aNewJob;
+}
+
+void printNodes(igraph_t* graph){
+	igraph_vector_t gtypes, vtypes, etypes;
+	igraph_strvector_t gnames, vnames, enames;
+	long int i;
+	
+	long int j;
+	
+	igraph_vector_init(&gtypes, 0);
+	igraph_vector_init(&vtypes, 0);
+	igraph_vector_init(&etypes, 0);
+	igraph_strvector_init(&gnames, 0);
+	igraph_strvector_init(&vnames, 0);
+	igraph_strvector_init(&enames, 0);
+	
+	igraph_cattribute_list(graph, &gnames, &gtypes, &vnames, &vtypes, &enames, &etypes);
+	
+	for(i = 0; i < igraph_vcount(graph); i++){
+		std::cout << "ID: " << VAN(graph, "id", i) << "," << VAN(graph, "runtime" ,i) << "\n";
+	}
+	
+	igraph_vector_destroy(&gtypes);
+	igraph_vector_destroy(&vtypes);
+	igraph_vector_destroy(&etypes);
+	igraph_strvector_destroy(&gnames);
+	igraph_strvector_destroy(&enames);
+	igraph_strvector_destroy(&vnames);
+	
+}
+
+void printEdges(igraph_t* graph){
+	igraph_vector_t gtypes, vtypes, etypes;
+	igraph_strvector_t gnames, vnames, enames;
+	long int i;
+	
+	long int j;
+	
+	igraph_vector_init(&gtypes, 0);
+	igraph_vector_init(&vtypes, 0);
+	igraph_vector_init(&etypes, 0);
+	igraph_strvector_init(&gnames, 0);
+	igraph_strvector_init(&vnames, 0);
+	igraph_strvector_init(&enames, 0);
+	
+	igraph_es_t edges;
+	igraph_es_all(&edges, IGRAPH_EDGEORDER_FROM);
+	igraph_eit_t edgeIter;
+	igraph_eit_create(graph, edges, &edgeIter);
+	
+	
+	
+	
+	while (! IGRAPH_EIT_END(edgeIter)){
+		igraph_integer_t eid = IGRAPH_EIT_GET(edgeIter);
+		igraph_integer_t from;
+		igraph_integer_t to;
+		igraph_edge(graph, eid, &from, &to);
+		
+		std::cout << VAN(graph, "id", from) << "," << VAN(graph, "id", to) << "\n";
+		
+		IGRAPH_EIT_NEXT(edgeIter);
+	}
+	
+	igraph_vector_destroy(&gtypes);
+	igraph_vector_destroy(&vtypes);
+	igraph_vector_destroy(&etypes);
+	igraph_strvector_destroy(&gnames);
+	igraph_strvector_destroy(&enames);
+	igraph_strvector_destroy(&vnames);
+	igraph_es_destroy(&edges);
+}
