ea20cc240dc9ec4fcfcf5b2f791c613f57755bf6
Namohysip <higa.noah@gmail.com>
2016-09-29 00:11:06 -1000

    Update PermutationMaker.cpp
    Added more methods; the core of the program, exhaustivePermutation, is now in.

--- C/PermutationMaker.cpp
+++ C/PermutationMaker.cpp
@@ -1,6 +1,10 @@
 #include <igraph.h>
 #include <algorithm>
 #include <iostream>
+#include <vector>
+
+void printNodes(igraph_t *);
+void printEdges(igraph_t *);
 
 void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 	int newRuntime = VAN(G, "runtime", node1) + VAN(G, "runtime", node2);
@@ -16,7 +20,6 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 	igraph_vit_create(G, out2, &outIter2);
 	igraph_vit_create(G, in2, &inIter2);
 	while (! IGRAPH_VIT_END(outIter2)){
-			
 		igraph_integer_t child = IGRAPH_VIT_GET(outIter2);
 		/*Delete the edge (node2, child) and then add edge (node1, child) */
 		igraph_es_t edel;
@@ -25,7 +28,11 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 		igraph_es_1(&edel, eid);
 		igraph_delete_edges(G, edel);
 		if (! ( (int) node1 == (int) child)){ //ignore self-loops
-			igraph_add_edge(G, node1, child); 
+		igraph_integer_t exists;
+		igraph_get_eid(G, &exists, node1, child, (igraph_bool_t) true, (igraph_bool_t) false);
+			if (exists == -1) {
+				igraph_add_edge(G, node1, child); 
+			}
 		}
 		IGRAPH_VIT_NEXT(outIter2); //go to next out-vertex
 		igraph_es_destroy(&edel);
@@ -37,12 +44,14 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 		igraph_integer_t eid;
 		igraph_get_eid(G, &eid, parent, node2, (igraph_bool_t) true, (igraph_bool_t) false);
 		igraph_es_1(&edel, eid);
-		
-	    std::cout << eid << "\n";
 		igraph_delete_edges(G, edel);
 		
 		if (! ( (int) node1 == (int) parent)){ //ignore self-loops
-			igraph_add_edge(G, node1, parent); 
+		igraph_integer_t exists;
+		igraph_get_eid(G, &exists, parent, node1, (igraph_bool_t) true, (igraph_bool_t) false);
+			if (exists == -1) {
+				igraph_add_edge(G, parent, node1); 
+			}
 		}
 		IGRAPH_VIT_NEXT(inIter2); //go to next in-vertex
 		igraph_es_destroy(&edel);
@@ -58,4 +67,113 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 	igraph_vit_destroy(&inIter2);
 	igraph_vs_destroy(&out2);
 	igraph_vs_destroy(&in2);
+}
+
+bool isSameJob (igraph_integer_t node1, igraph_integer_t node2, igraph_t * graph1, igraph_t * graph2){
+	if ( VAN(graph1, "runtime", node1) != VAN(graph2, "runtime", node2)) {
+		return false;
+	}
+	if ( VAN(graph1, "id", node1) != VAN(graph2, "id", node2)) {
+		return false;
+	}
+	return true;
+}
+
+/*This function checks if the two graphs have identical jobs based on the edges.
+ If every edge matches exactly, then it's the same graph, because all of the jobs
+ have the same ids and runtimes for every endpoint, and are arranged the same way. */
+bool checkAllNodes (igraph_t * graph1, igraph_t * graph2) {
+	if (igraph_vcount(graph1) != igraph_vcount(graph2)) {
+		return false;
+	}
+	if (igraph_ecount(graph1) != igraph_ecount(graph2)) {
+		return false;
+	}
+	igraph_es_t edges1;
+	igraph_es_t edges2;
+	igraph_eit_t iter1;
+	igraph_eit_t iter2;
+	
+	igraph_es_all(&edges1, IGRAPH_EDGEORDER_FROM);
+	igraph_es_all(&edges2, IGRAPH_EDGEORDER_FROM);
+	igraph_eit_create(graph1, edges1, &iter1);
+	igraph_eit_create(graph2, edges2, &iter2);
+	while( ! IGRAPH_EIT_END(iter1) ) {
+		bool found = false;
+		IGRAPH_EIT_RESET(iter2);
+		while (! IGRAPH_EIT_END(iter2) ) {
+			igraph_integer_t to1, from1, to2, from2;
+			igraph_edge(graph1, IGRAPH_EIT_GET(iter1), &from1, &to1);
+			igraph_edge(graph2, IGRAPH_EIT_GET(iter2), &from2, &to2);
+			if( isSameJob(from1, from2, graph1, graph2)) {
+				if (isSameJob(to1, to2, graph1, graph2)) {
+					found = true;
+					break;
+				}
+			}
+			IGRAPH_EIT_NEXT(iter2);
+		}
+		IGRAPH_EIT_NEXT(iter1);
+		if (!found) {
+			igraph_es_destroy(&edges1);
+			igraph_es_destroy(&edges2);
+			igraph_eit_destroy(&iter1);
+			igraph_eit_destroy(&iter2);
+			return false;
+		}
+	}
+	
+	igraph_es_destroy(&edges1);
+	igraph_es_destroy(&edges2);
+	igraph_eit_destroy(&iter1);
+	igraph_eit_destroy(&iter2);
+	return true;
+}
+
+bool addWithoutDuplicates( std::vector<igraph_t *> * graphs, igraph_t * toAdd) {
+	for(std::vector<igraph_t*>::iterator it = graphs->begin(); it != graphs->end(); ++it){
+		if (checkAllNodes( toAdd, *it)){
+			return false;
+		}
+	}
+	graphs->push_back(toAdd);
+	return true;
+}
+
+
+void exhaustivePerm(std::vector<igraph_t *> * graphs, igraph_t * graph){
+	if(igraph_vcount(graph)< 2){
+		return; //base case -- only one node left
+	}
+	int nodes = igraph_vcount(graph);
+	for(int i = 0; i < nodes - 1; i++){
+		for(int j = i+1; j < nodes; j++){
+			igraph_t * newGraph = new igraph_t;
+			igraph_copy(newGraph, graph);
+			igraph_integer_t gi = i;
+			igraph_integer_t gj = j;
+			combine(newGraph, gi, gj);
+			igraph_bool_t legal;
+			igraph_is_dag(newGraph, &legal);
+			if((bool) legal){
+				if(addWithoutDuplicates(graphs, newGraph)){
+					exhaustivePerm(graphs, newGraph);
+				}
+				else{
+					igraph_destroy(newGraph);
+				}
+			}
+			else {
+				igraph_destroy(newGraph);
+			}
+		}
+	}
+}
+
+
+std::vector<igraph_t *> exhaustivePermStart(igraph_t * graph){
+	std::vector<igraph_t *> graphs;
+	graphs.push_back(graph);
+	exhaustivePerm(&graphs, graph);
+	return graphs;
 }
