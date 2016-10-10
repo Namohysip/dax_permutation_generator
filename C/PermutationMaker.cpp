cda45fa7ec2caf94d2eca9c1733a82005a5ec1b6
Namohysip <higa.noah@gmail.com>
2016-10-09 18:18:14 -1000

    Added hash functions
    Added hash functions and comments for the functions.

--- C/PermutationMaker.cpp
+++ C/PermutationMaker.cpp
@@ -5,9 +5,12 @@
 #include <string.h>
 #include <fstream>
 #include <ctime>
+#include "sha256.h"
 
 void printNodes(igraph_t *);
 void printEdges(igraph_t *);
+
+int permCount = 0;
 
 void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 	double newRuntime = VAN(G, "runtime", node1) + VAN(G, "runtime", node2);
@@ -33,7 +36,7 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 		if (! ( (int) node1 == (int) child)){ //ignore self-loops
 		igraph_integer_t exists;
 		igraph_get_eid(G, &exists, node1, child, (igraph_bool_t) true, (igraph_bool_t) false);
-			if (exists == -1) {
+			if (exists == -1) { //make sure the edge doesn't already exist
 				igraph_add_edge(G, node1, child); 
 			}
 		}
@@ -52,7 +55,7 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 		if (! ( (int) node1 == (int) parent)){ //ignore self-loops
 		igraph_integer_t exists;
 		igraph_get_eid(G, &exists, parent, node1, (igraph_bool_t) true, (igraph_bool_t) false);
-			if (exists == -1) {
+			if (exists == -1) { //make sure the edge doesn't already exist.
 				igraph_add_edge(G, parent, node1); 
 			}
 		}
@@ -71,6 +74,8 @@ void combine(igraph_t * G, igraph_integer_t node1, igraph_integer_t node2){
 	igraph_vs_destroy(&in2);
 }
 
+/*Determines whether the two given nodes in the two different graphs are the same, in that
+they have the same runtime and id attribute values. */
 bool isSameJob (igraph_integer_t node1, igraph_integer_t node2, igraph_t * graph1, igraph_t * graph2){
 	if ( VAN(graph1, "runtime", node1) != VAN(graph2, "runtime", node2)) {
 		return false;
@@ -132,6 +137,7 @@ bool checkAllNodes (igraph_t * graph1, igraph_t * graph2) {
 	return true;
 }
 
+/*Adds an igraph to the UN-HASHED list of igraphs used. Without adding duplicate graphs.*/
 bool addWithoutDuplicates( std::vector<igraph_t *> * graphs, igraph_t * toAdd) {
 	for(std::vector<igraph_t*>::iterator it = graphs->begin(); it != graphs->end(); ++it){
 		if (checkAllNodes( toAdd, *it)){
@@ -142,7 +148,8 @@ bool addWithoutDuplicates( std::vector<igraph_t *> * graphs, igraph_t * toAdd) {
 	return true;
 }
 
-
+/*Exhaustively permutes all possible clusterings of the igraph. Has no time limit; it will 
+go until completion. */
 void exhaustivePerm(std::vector<igraph_t *> * graphs, igraph_t * graph){
 	if(igraph_vcount(graph)< 2){
 		return; //base case -- only one node left
@@ -166,15 +173,20 @@ void exhaustivePerm(std::vector<igraph_t *> * graphs, igraph_t * graph){
 				}
 				else{
 					igraph_destroy(newGraph);
+					delete(newGraph);
 				}
 			}
 			else {
 				igraph_destroy(newGraph);
+				delete(newGraph);
 			}
 		}
 	}
 }
 
+/*Same as exhaustive perm, but limited by both a time limit or cap on how many 
+ graphs to permute. Once either limit is reached, no more permutations will be made,
+ and the recursive function will go back to the top level and terminate. */
 bool timedExhaustivePerm(std::vector<igraph_t *> * graphs, igraph_t * graph, double time, clock_t start, int goal){
 	if(igraph_vcount(graph)< 2){
 		return true; //base case -- only one node left
@@ -214,6 +226,8 @@ bool timedExhaustivePerm(std::vector<igraph_t *> * graphs, igraph_t * graph, dou
 	return true;
 }
 
+
+/*This is what should be called when starting any exhaustive permutation that does NOT use hashing. */
 std::vector<igraph_t *> * exhaustivePermStart(igraph_t * graph, bool timed = false, double seconds = 0, int goal = 0){
 	std::vector<igraph_t *> * graphs = new std::vector<igraph_t *>;
 	graphs->push_back(graph);
@@ -227,6 +241,44 @@ std::vector<igraph_t *> * exhaustivePermStart(igraph_t * graph, bool timed = fal
 	return graphs;
 }
 
+/*Generates and returns what the given graph will look like in its DAX .xml format. */
+std::string dagToDAXStr(igraph_t * graph){
+	std::string result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n<adag xmlns=\"http://pegasus.isi.edu/schema/DAX\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://pegasus.isi.edu/schema/DAX http://pegasus.isi.edu/schema/dax-2.1.xsd\" version=\"2.1\" count=\"1\" index=\"0\" name=\"NAME\" jobCount=\"";
+		result += std::to_string(igraph_vcount(graph));
+		result += std::string("\" fileCount=\"0\" childCount=\"");
+		result += std::to_string(igraph_vcount(graph) - 1) + "\">\n\n";
+		
+	for(int i = 0; i < igraph_vcount(graph); i++){
+		result += "<job id =\"";
+		result += std::string(VAS(graph, "id", i));
+		result += "\" namespace=\"NAMESPACE\" name=\"Task\" version=\"1.0\" runtime=\"" + std::to_string(VAN(graph, "runtime", i)) + "\">\n\n";
+	}
+	result += "\n";
+	for (int i = 0; i < igraph_vcount(graph); i++) {
+			igraph_vs_t parents;
+			igraph_vit_t parIter;
+		
+			igraph_integer_t node = i;
+			igraph_vs_adj(&parents, node, IGRAPH_IN);
+			igraph_vit_create(graph, parents, &parIter);
+			
+			if(! IGRAPH_VIT_END(parIter)){
+				result += "<child ref=\"" + std::string(VAS(graph, "id", i)) + "\">";
+				while(! IGRAPH_VIT_END(parIter)){
+					result += "\n  <parent ref=\"" + std::string(VAS(graph, "id", IGRAPH_VIT_GET(parIter))) + "\" />";
+					IGRAPH_VIT_NEXT(parIter);
+				}
+				result += "\n</child>\n";
+			}
+			igraph_vs_destroy(&parents);
+			igraph_vit_destroy(&parIter);
+		}
+		result += "\n</adag>";
+		return result;
+}
+
+
+/*Converts the given graph into a DAX .xml format and then outputs the result. */
 void dagToDAX(igraph_t * graph, std::string filebase, int permCount){
 	std::string name = filebase + "_p" + std::to_string(permCount) + ".xml";
 	std::ofstream permFile(name);
@@ -273,9 +325,10 @@ void dagToDAX(igraph_t * graph, std::string filebase, int permCount){
 		permFile << "\n</adag>";
 		permFile.close();
 	}
-	else std::cout << "Error writing file";
+	else std::cerr << "Error writing file";
 }
 
+/*Outputs all of the graphs in the given list of igraphs, using the file base for the names.*/
 void outputDAX(std::vector<igraph_t *> * graphs, std::string fileBase){
 	int i = 0;
 	std::cout << "Writing out ";
@@ -288,4 +341,82 @@ void outputDAX(std::vector<igraph_t *> * graphs, std::string fileBase){
 			std::cout << "Wrote out " << i << " files.\n";
 		}
 	}
+}
+
+/*Same as outputDAX, but directly using the string given.*/
+void outputDAXStr(std::string graph, std::string filebase){
+	std::string name = filebase + "_p" + std::to_string(permCount) + ".xml";
+	std::ofstream permFile(name);
+	permCount++;
+	if(permFile.is_open()) {
+		permFile << graph;
+	}
+	else {
+		std::cout << "error writing file";
+	}
+	permFile.close();
+}
+
+/*The hash equivalent of "Add without duplicates" and "dagToDAX" put together.
+ First it computes the DAX format of the graph and hashes it, and then checks if there are
+ duplicates in the given list. If there aren't, it outputs that file and returns true. Otherwise,
+ no file is output, nothing is added to the list, and it returns false. */
+bool addHashAndOutput(std::vector<std::string> *graphs, igraph_t * newGraph, std::string fileBase){
+	std::string daxString = dagToDAXStr(newGraph);
+	std::string hash = sha256(daxString);
+	for(std::vector<std::string>::iterator it = graphs->begin(); it != graphs->end(); ++it){
+		if (strcmp(hash.c_str(), it->c_str()) == 0){
+			return false;
+		}
+	}
+	graphs->push_back(hash);
+	outputDAXStr(daxString, fileBase);
+	return true;
+}
+
+/*The hash equivalent of timedExhaustivePerm. */
+bool timedExhaustivePermHash(std::vector<std::string> * graphs, igraph_t * graph, double time, clock_t start, int goal, std::string fileBase){
+	if(igraph_vcount(graph)< 2){
+		return true; //base case -- only one node left
+	}
+	if (time < double(clock() - start) / CLOCKS_PER_SEC || graphs->size() > goal){ //time-up case -- return and wrap things up
+		return false;
+	}
+	
+	if(graphs->size() % 1000 == 0){
+		std::cout << "Made these many permutations: " << graphs->size() << "\n";
+	}
+	int nodes = igraph_vcount(graph);
+	for(int i = 0; i < nodes - 1; i++){
+		for(int j = i+1; j < nodes; j++){
+			igraph_t * newGraph = new igraph_t;
+			igraph_copy(newGraph, graph);
+			igraph_integer_t gi = i;
+			igraph_integer_t gj = j;
+			combine(newGraph, gi, gj);
+			igraph_bool_t legal;
+			igraph_is_dag(newGraph, &legal);
+			if((bool) legal){
+				if(addHashAndOutput(graphs, newGraph, fileBase)){
+					if(! timedExhaustivePermHash(graphs, newGraph, time, start, goal, fileBase)){
+						return false; //ran out of time
+					}
+				}
+			}
+			igraph_destroy(newGraph);
+			delete(newGraph);
+		}
+	}
+	return true;
+}
+
+/*The hash equivalent of TimedExhaustivePermStart. Also takes in the fileBase name for output, 
+since the output is done for each permutation after it is created, rather than after the entire
+function terminates. */
+std::vector<std::string> * exhaustivePermHashStart(igraph_t * graph, std::string fileBase, double seconds = 0, int goal = 0){
+	std::vector<std::string> * graphs = new std::vector<std::string>;
+	addHashAndOutput(graphs, graph, fileBase);
+		clock_t start = clock();
+		timedExhaustivePermHash(graphs, graph, seconds, start, goal, fileBase);
+	return graphs;
 }
