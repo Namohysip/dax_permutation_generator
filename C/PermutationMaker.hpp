ea13912c20e4c82309929ea9a551f2c835bd7449
Namohysip <higa.noah@gmail.com>
2016-11-22 21:39:41 -1000

    Added GlobalOptions struct
    Also changed a few parameters to make things look nicer thanks to the new struct.

--- C/PermutationMaker.hpp
+++ C/PermutationMaker.hpp
@@ -1,12 +1,23 @@
+#include <string>
+
 #ifndef PERMUTATIONMAKER_H
 #define PERMUTATIONMAKER_H
 
+struct GlobalOptions {
+	std::string fileBase;
+	int maxGraphs;
+	double timeLimit;
+	bool mergeChains;
+};
+
 void combine(igraph_t * , igraph_integer_t, igraph_integer_t);
 bool addWithoutDuplicates( std::vector<igraph_t *> * , igraph_t *);
-std::vector<igraph_t *> * exhaustivePermStart(igraph_t * graph, std::string filebase, bool timed = false, double seconds = 0 , int goal = 0);
+std::vector<igraph_t *> * exhaustivePermStart(igraph_t * graph);
 igraph_t * getImported();
-std::vector<std::string> * exhaustivePermHashStart(igraph_t * graph,  std::string fileBase, double seconds, int goal);
-std::vector<igraph_t *> * randomizedPerm(igraph_t * graph, double time, int max, std::string fileBase);
-int RandomizedPermEvenSpread(igraph_t * graph, int maxPerLevel, int depthLimit, int attempt_cap, std::string fileBase);
+std::vector<std::string> * exhaustivePermHashStart(igraph_t * graph);
+std::vector<igraph_t *> * randomizedPerm(igraph_t * graph);
+int RandomizedPermEvenSpread(igraph_t * graph, int maxPerLevel, int depthLimit, int attempt_cap);
+bool mergeAChain(igraph_t * graph);
+struct GlobalOptions * getConfig();
 
 #endif
